75b8d9539f72ebca84ef25cb7a4792645b407ed9
Aroldo Hernandez Armas <aroldohernandezarmas@gmail.com>
2017-08-11 23:29:57 +0200

    Refactor AnagramGenerator for thread safety.

--- src/main/java/AnagramGenerator.java
+++ src/main/java/AnagramGenerator.java
@@ -2,27 +2,25 @@ import java.util.HashSet;
 
 class AnagramGenerator {
 
-    private boolean[] mark;
-    private String base;
-    private HashSet<String> result = new HashSet<>();
-
     HashSet<String> generate(String base) throws IllegalArgumentException {
         if (base == null || base.length() == 0)
             throw new IllegalArgumentException("The input shouldn't be null or an empty string.");
         else {
-            this.base = base;
-            this.mark = new boolean[base.length()];
-            internalGenerator("");
+            HashSet<String> result = new HashSet<>();
+            internalGenerator(base, "", new boolean[base.length()], result);
             return result;
         }
     }
 
-    private void internalGenerator(String acc) { // base = "ana" [0, 0, 0]
-        if (acc.length() == base.length()) result.add(acc);
+    private void internalGenerator(String base, String acc, boolean[] mark, HashSet<String> result) { // base = "ana" [0, 0, 0]
+        if (acc.length() == base.length()) {
+            result.add(acc);
+            return;
+        }
         for (int i = 0; i < base.length(); i++) {
             if (!mark[i]) {
                 mark[i] = true;
-                internalGenerator(acc + base.charAt(i));
+                internalGenerator(base, acc + base.charAt(i), mark, result);
                 mark[i] = false;
             }
         }
